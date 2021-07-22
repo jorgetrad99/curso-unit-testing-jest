5202a1108a7da77a3a125166c8e10a84fc2cb34c
Jorge Trad <jetradl@gmail.com>
2021-07-22 17:20:16 -0500

    Implementing tests on promises

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -26,3 +26,39 @@ test('Probar un Callback', () => {
     expect(str).toBe('aloH');
   });
 });
+
+const revertString2 = (str) => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject(Error('Error'));
+    }
+    resolve(str.split('').reverse().join(''));
+  });
+};
+
+test('Probar una promesa', () => {
+  return revertString2('Hola').then((string) => {
+    expect(string).toBe('aloH');
+  });
+});
+
+test('Probar async/await', async () => {
+  const string = await revertString2('Hola');
+  expect(string).toBe('aloH');
+});
+
+afterEach(() => {
+  console.log('Después de cada prueba');
+});
+
+afterAll(() => {
+  console.log('Después de todas las pruebas');
+});
+
+beforeEach(() => {
+  console.log('Antes de cada prueba');
+});
+
+beforeAll(() => {
+  console.log('Antes de todas las pruebas');
+});
