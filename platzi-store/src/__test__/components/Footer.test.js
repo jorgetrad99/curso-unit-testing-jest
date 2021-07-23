c2f801cbc8884206c0e83bb507e50913114e50b5
Jorge Trad <jetradl@gmail.com>
2021-07-22 21:42:16 -0500

    Mocks Creation

--- platzi-store/src/__test__/components/Footer.test.js
+++ platzi-store/src/__test__/components/Footer.test.js
@@ -1,10 +1,15 @@
+import 'jsdom-global/register';
 import React from 'react';
 import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+  const footer = mount(<Footer />);
+
   test('Render del componente Footer', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+  test('Render del titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
