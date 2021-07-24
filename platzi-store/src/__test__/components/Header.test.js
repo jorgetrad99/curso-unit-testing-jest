51e7b26fac4acb0a5eb2856145e584127197ae0b
Jorge Trad <jetradl@gmail.com>
2021-07-23 22:58:06 -0500

    Snapshot

--- platzi-store/src/__test__/components/Header.test.js
+++ platzi-store/src/__test__/components/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
 import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
 
@@ -8,7 +9,7 @@ describe('<Header />', () => {
     const header = shallow(
       <ProviderMock>
         <Header />
-      </ProviderMock>,
+      </ProviderMock>
     );
     expect(header.length).toEqual(1);
   });
@@ -16,9 +17,19 @@ describe('<Header />', () => {
     const header = mount(
       <ProviderMock>
         <Header />
-      </ProviderMock>,
+      </ProviderMock>
     );
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
   });
 });
 
+describe('Header Snapshot', () => {
+  test('Comprobar el Snapshot de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
+  });
+});
